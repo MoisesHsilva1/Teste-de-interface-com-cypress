199a5c92f8614e581e8bf732ed00aff3c208ca8f
MoisésHsilva1 <moihgm67@gmail.com>
2024-04-10 12:56:41 -0300

    Testando interface

--- cypress/e2e/exemplos/testTilter.cy.js
+++
@@ -1,7 +0,0 @@
-describe ('Testando to-do-list', () => {
-it ('Entrando na pagina de teste', () => {
-    cy.visit('https://example.cypress.io/todo');
-    cy.contains('todos').should('to.have.lengthOf', 1)
-   });
-});
- 
