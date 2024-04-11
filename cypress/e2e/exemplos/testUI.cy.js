a5bb5eaf3f2be2cdd962a70d6d498fc6d8522455
MoisésHsilva1 <moihgm67@gmail.com>
2024-04-10 22:03:28 -0300

    Testando interfaces

--- cypress/e2e/exemplos/testUI.cy.js
+++ cypress/e2e/exemplos/testUI.cy.js
@@ -3,9 +3,25 @@ describe ('Testando to-do-list', () => {
       cy.visit('https://todomvc.com/examples/react/dist/');
  });
        
-   it ('Entrando na pagina de teste', () => {
+   it ('Vericando Titulo', () => {
       cy.get('.header').contains('todos').should('to.have.lengthOf', 1)
    });
 
+   it('Verificando Input', () => {
+      cy.get('[data-testid="text-input"]').should('to.have.lengthOf', 1)
+   });
+
+   it('Verificando instrução de uso', () => {
+      cy.contains('Double-click to edit a todo').should('be.visible')
+   });
+
+   it('Verificando criado do projeto',() => {
+      cy.contains('Created by the TodoMVC Team').should('be.visible')
+   });    
+
+   it('Verificando Botao para page inicial', () => {
+      cy.get('a').contains('TodoMVC').click()
+      cy.location('href').should('visit','https://todomvc.com/')
+   });
 });
  
