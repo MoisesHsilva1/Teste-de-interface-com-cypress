2451eb077fb60cb0e08233477e9ba3a75674aa82
MoisésHsilva1 <moihgm67@gmail.com>
2024-04-12 17:02:07 -0300

    add teste de selecionar tarefa

--- cypress/e2e/exemplos/TesteInuput.cy.js
+++ cypress/e2e/exemplos/TesteInuput.cy.js
@@ -1,12 +1,18 @@
-const { it } = require("mocha");
-
 describe('Testando o Input da pagina', () => {
-    beforeEach(() => {
-        cy.visit('https://todomvc.com/examples/react/dist/')
-});
-  
-        Cypress.Commands.add('Addtarefa', (tarefa) => {
+   it('Testando comportamento de usuario adiciando e removendo terefa', () => {
+       cy.visit('https://todomvc.com/examples/react/dist/')
+
+        cy.log('Adicinando tarefa no input')
         cy.get('[data-testid="text-input"]').type('Tarefa {enter}')
-        });
-});
+
+        cy.log('Selecinando tarefa')
+        cy.get('[type="checkbox"]').check()
+
+
+         cy.get('[data-testid="todo-item-label"]').dblclick()   
+         cy.get('[data-testid="todo-item-label"]').clear()   
+        
+       
+   })
+})
 
