3e2b776a5a6f133f304b272d73ecaeb49827985e
MoisésHsilva1 <moihgm67@gmail.com>
2024-04-13 01:38:06 -0300

    Alterando nome dos arquivos

---
+++ cypress/e2e/teste/Test-Taks-Completed.cy.js
@@ -0,0 +1,32 @@
+describe('Testando o Input da pagina', () => {
+  beforeEach(() => {
+      cy.visit('https://todomvc.com/examples/react/dist/')
+    })
+    
+it('Testando comportamento de usuario adiciando tarefas completas e removendo', () => {
+
+        cy.log('Funcao de adicionar tarefa')
+        cy.tarefa()
+        
+        cy.log('Selecinando tarefa como concluida')
+        cy.get('[type="checkbox"]').check()
+
+        cy.log('Selecionando o botao tarefas completas')
+        cy.get('a[href="#/completed"]').click()
+
+        cy.log('Selecionando o botao de tarefas ativas')
+        cy.get('a[href="#/active"]').click()
+
+       cy.log('Selecionando o botao de todas as tarefas')
+       cy.get('a[href="#/"] ').click()
+
+       cy.log('Removendo apenas uma Tarefa selecionda')
+       cy.get('[data-testid="todo-item-button"]').click({force: true})
+
+       cy.log('selecionando botao de remover todas as tarefas completas')
+       cy.tarefa()
+       cy.tarefaSelecionada()
+       cy.get('.clear-completed').click()
+   })
+})
+
